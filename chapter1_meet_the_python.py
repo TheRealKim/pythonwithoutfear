2c1a7c56edb9c040223a23d9ad09e04b034cef6f
TheRealKim <therealkim417@hotmail.com>
2023-10-04 19:22:54 +0200

    chapter1

---
+++ chapter1_meet_the_python.py
@@ -0,0 +1,12 @@
+# Dealing with Ends of Lines
+total_amount = (1 + 2
+                + 3 + 4)
+print(total_amount)
+
+my_str = 'I am typing a very long line \
+line of code'
+print(my_str)
+
+my_str = 'I\'m using a lot of backslashes (\\) \
+ in this very long line of code'
+print(my_str)
